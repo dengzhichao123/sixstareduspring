1aa694c23b38c5abbc40a0abed9a7f59156a525f
Mr.Tab <3535662992@qq.com>
2020-06-12 22:03:42 +0800

    六星教育阿超老师Spring5.3.0版 测试类

---
+++ ac-test/src/main/java/com/sixstar/one/test/TestOne.java
@@ -0,0 +1,49 @@
+package com.sixstar.one.test;
+
+import com.sixstar.one.Acbean;
+import com.sixstar.one.Xacbean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @ClassName:
+ * @Description: 说明
+ * @Author: 六星教育-阿超老师
+ * @CreateDate: 2020/6/12    22:02
+ * @Version: 1.0
+ */
+public class TestOne {
+
+	public void test1(){
+		//加载Spring-xml配置文件   并且创建Bean
+		ApplicationContext application1 = new ClassPathXmlApplicationContext("applicationContextOne.xml");
+
+		//获取注册到容器中的组件name集合，遍历打印
+		String[] beanDefinitionNames = application1.getBeanDefinitionNames();
+		for (String name : beanDefinitionNames){
+			System.out.println(name+"");}
+
+		//从容器中获取bean
+		Xacbean xo = (Xacbean) application1.getBean("xacbean");
+		System.out.println(xo);
+	}
+
+	public void test2(){
+		System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+		//加载Spring注解配置类
+		AnnotationConfigApplicationContext application2 = new AnnotationConfigApplicationContext(Acbean.class);
+
+		//获取注册到容器中的组件name集合，遍历打印
+		String[] beanDefinitionNames = application2.getBeanDefinitionNames();
+		for (String name : beanDefinitionNames){
+			System.out.println(name+"得到");}
+
+		//从容器中获取bean
+		Xacbean xacbean = (Xacbean) application2.getBean("xacbean");
+		Xacbean xacbean1 = (Xacbean) application2.getBean("xacbean");
+		System.out.println(xacbean==xacbean1);
+		System.out.println(xacbean);
+		System.out.println(xacbean1);
+	}
+}
