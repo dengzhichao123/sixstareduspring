cb49a7aec3d5884980101c59cacffea3b843a1ef
Mr.Tab <3535662992@qq.com>
2020-06-12 20:04:01 +0800

    spring5-3-0

---
+++ ac-test/src/main/java/Xacbean.java
@@ -0,0 +1,57 @@
+import org.springframework.beans.factory.annotation.Value;
+
+/**
+ * @ClassName: Xacbean
+ * @Description: 说明 xml注册bean形式
+ * @Author: 六星教育-阿超老师
+ * @CreateDate: 2020/6/3    13:47
+ * @Version: 1.0
+ */
+public class Xacbean {
+	private String name;
+//SpEL表达式
+	private Integer age;
+
+	private  String sex;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public Xacbean(){}
+
+	public Xacbean(String name,Integer age,String sex){
+		this.name = name;
+		this.age = age;
+		this.sex = sex;
+	}
+
+	@Override
+	public String toString() {
+		return "Xacbean{" +
+				"name='" + name + '\'' +
+				", age=" + age +
+				", sex='" + sex + '\'' +
+				'}';
+	}
+}
