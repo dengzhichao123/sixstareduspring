087c499d9b854a11eda0e0349c77e30cc6fea037
Mr.Tab <3535662992@qq.com>
2020-06-15 17:06:02 +0800

    删除

--- ac-springtest/src/main/java/com/six/ioc/one/one/UserConfig.java
+++
@@ -1,9 +0,0 @@
-/**
- * @ClassName:
- * @Description: 说明
- * @Author: 六星教育-阿超老师
- * @CreateDate: 2020/6/13    21:52
- * @Version: 1.0
- */
-public class UserConfig {
-}
--- ac-springtest/src/main/java/com/six/ioc/one/two/controlloer/UserControlloer.java
+++
@@ -1,9 +0,0 @@
-/**
- * @ClassName:
- * @Description: 说明
- * @Author: 六星教育-阿超老师
- * @CreateDate: 2020/6/13    22:11
- * @Version: 1.0
- */
-public class UserControlloer {
-}
